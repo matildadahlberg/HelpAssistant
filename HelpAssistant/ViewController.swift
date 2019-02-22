86d24f222eb121f15911642c0acb029500aab780
Matilda Dahlberg <info.matildadahlberg@gmail.com>
2019-02-22 14:45:28 +0100

    Initial Commit

---
+++ HelpAssistant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelpAssistant
+//
+//  Created by Matilda Dahlberg on 2019-02-22.
+//  Copyright © 2019 Matilda Dahlberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
