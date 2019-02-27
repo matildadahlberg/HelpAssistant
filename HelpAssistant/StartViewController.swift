c772f3ce72c85a603ef0bb7ffcfdf3138f31c518
Matilda Dahlberg <info.matildadahlberg@gmail.com>
2019-02-27 12:44:31 +0100

    Add: Image to assets, another view in storyboard and a StartViewController

---
+++ HelpAssistant/StartViewController.swift
@@ -0,0 +1,19 @@
+//
+//  StartViewController.swift
+//  HelpAssistant
+//
+//  Created by Matilda Dahlberg on 2019-02-27.
+//  Copyright © 2019 Matilda Dahlberg. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+  
+    
+}
